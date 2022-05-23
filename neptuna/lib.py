8c774b1a3104fadaca7a41d6268a1047a3aab4ae
joaosantos <joaovps19@gmail.com>
2022-05-23 16:17:15 -0300

    Initial Push

---
+++ neptuna/lib.py
@@ -0,0 +1,7 @@
+def try_me(password):
+
+    if password == 62442:
+        return 'Welcome to the Wizard World'
+    else:
+        res = "Mr. Moony presents his compliments and begs you to keep your abnormally large nose out of other people's business."
+        return res
